28db22a455979df37e77daccacecede0726883d8
kareem22222 <kareemshaik2002@gmail.com>
2026-07-13 12:33:45 +0530

    redirect to main section

--- frontend/src/components/ProfilePage.vue
+++ frontend/src/components/ProfilePage.vue
@@ -131,7 +131,7 @@ function goBack() {
         </template>
         <template v-else>
           <button type="button" class="dash-btn dash-btn--primary" :disabled="saving" @click="save">
-            {{ saving ? 'Saving...' : 'Save' }}
+            {{ saving ? 'Saving...' : 'Save changes' }}
           </button>
           <button type="button" class="dash-btn dash-btn--outline" :disabled="saving" @click="cancelEdit">Cancel</button>
         </template>
